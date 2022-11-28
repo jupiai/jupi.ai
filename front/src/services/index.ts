6a4154d469f7bd94235c6c67b27f5db42cb063e6
apietryga <antek.pietryga@gmail.com>
2022-11-29 00:22:29 +0100

    repair host

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -2,9 +2,10 @@ import axios from "axios"
 
 const local = "http://127.0.0.1:3300"
 const prod = "http://54.38.52.169:8080"
-const host = import.meta.env.VITE_ENV === 'local' ? local : prod;
+// const host = import.meta.env.VITE_ENV === 'local' ? local : prod;
+const host = ['127.0.0.1', 'localhost'].includes(location.hostname) ? local : prod;
 
-console.log('VITE_ENV:', import.meta.env.VITE_ENV )
+console.log( { host } )
 
 export default {
   get: async ( url: string ) => {
