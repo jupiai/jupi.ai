509a85f36f87343e08982e56c422ab5927c7967b
apietryga <antek.pietryga@gmail.com>
2022-11-29 00:15:19 +0100

    test

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -4,10 +4,10 @@ const local = "http://127.0.0.1:3300"
 const prod = "http://54.38.52.169:8080"
 const host = import.meta.env.VITE_ENV === 'local' ? local : prod;
 
-console.log('TOTOTO:', import.meta?.env?.VITE_ENV )
+console.log('VITE_ENV:', import.meta.env.VITE_ENV )
 
 export default {
-  get: async (url: string) => {
+  get: async ( url: string ) => {
     return ( await axios.get( host + '/api/' + url ) ).data
   } 
 }
