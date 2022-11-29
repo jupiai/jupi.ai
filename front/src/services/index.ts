e33ef252c682f09d4a196f4b076efe60a233f4ca
apietryga <antek.pietryga@gmail.com>
2022-11-29 17:01:24 +0100

    proxied

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+
+const proxy = "https://proxy.cors.sh/"
 const local = "http://127.0.0.1:3300"
-const prod = "http://54.38.52.169:8080"
+const prod = proxy + "http://54.38.52.169:8080"
 // const host = import.meta.env.VITE_ENV === 'local' ? local : prod;
 const host = ['127.0.0.1', 'localhost'].includes(location.hostname) ? local : prod;
 
