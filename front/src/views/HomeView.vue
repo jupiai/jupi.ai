e0d3c70043785c6f075a3ac8a4f905155451193b
apietryga <antek.pietryga@gmail.com>
2022-11-29 21:55:34 +0100

    loading animation

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -8,6 +8,7 @@
     image_src: string
   }
 
+  // set postheight
   const appHeight = () => {
       const doc = document.documentElement
       doc.style.setProperty('--app-height', `${window.innerHeight}px`)
@@ -21,14 +22,33 @@
         posts : [] as Post[],
         index : 0,
         touchStartPoint: 0,
+        bottomExhoust: false,
+        topExhoust: false
       }
     },
     methods : {
-      async loadPosts () {
-        const res: Post[] = await services.get('posts')
-        this.posts.push( ...res )
+      async loadBottom(){
+        if(this.bottomExhoust){ return }
+        this.bottomExhoust = true
+        this.index = this.posts.length 
+        setTimeout( async ()=>{ 
+          window.scrollTo(0,  document.body.scrollHeight) 
+          this.posts.push( ...await services.get('posts') )
+          setTimeout(()=>{ 
+            window.scrollTo(0,  this.index * innerHeight) 
+            this.bottomExhoust = false      
+          }, 100)
+        }, 100)
+      }, 
+      async loadTop(){
+        if(this.topExhoust){ return }
+        this.index = 0
+        this.topExhoust = true
+        this.posts = await services.get('posts')
+        this.topExhoust = false
       },
       smartScrolling ( e : WheelEvent | TouchEvent) {
+        if( this.topExhoust || this.bottomExhoust ){ return }
         let direction = 0;
         if( e.constructor === TouchEvent ){
           if(!this.touchStartPoint){ return }
@@ -38,21 +58,31 @@
         }
         this.touchStartPoint = 0
         this.index = direction > 0 ? ++this.index : --this.index;
+        switch ( this.index ) {
+          case -1: 
+            this.index = 0; 
+            this.loadTop(); 
+            break;
+          case this.posts.length: 
+            this.loadBottom(); 
+            break;
+        }
         window.scrollTo(0,  this.index * innerHeight)
       },
       touchStart(e: TouchEvent){
         this.touchStartPoint = e.touches[0].clientY
       },
     },
-    mounted () {
-      this.loadPosts()
+    async mounted () {
+      this.posts.push( ...await services.get('posts') )
     }
   })
 </script>
 
 <template>
   <main @wheel.prevent="smartScrolling" @touchmove.prevent="smartScrolling" @touchstart.prevent="touchStart">
-    <loading v-if="(posts.length === 0)" class="loadingWrapper" />
+    <loading class="postsLoading" :class="{ active: topExhoust }" />
+    <loading v-if="posts.length === 0" class="loadingWrapper" />
     <div v-else v-for="post of posts" class="post">
       <div class="content">
         <img :src="post.image_src" :alt="post.title">
@@ -62,18 +92,14 @@
         <p>{{ post.author }}</p>
       </div>
     </div>
+    <loading class="postsLoading" :class="{ active: bottomExhoust }" />
   </main>
 </template>
 
 <style lang="scss" scoped>
   .post{
     border:10px groove blue;
-    // height:100vh;
-    // min-height:fill-avalible;
-    // min-height: -webkit-fill-available;
     height: var(--app-height);
-
-
     position: relative;
     .content{
       border:2px dashed yellow;
@@ -99,4 +125,22 @@
     justify-content: center;
     align-items: center;
   }
+
+  .postsLoading{
+    display:flex;
+    justify-content: center;
+    padding:1rem;
+
+    position:relative;
+    height:0;
+    padding:0;
+    opacity:0;
+    transition:.2s;
+    &.active{
+      height:unset;
+      padding:1rem;
+      opacity:1;
+    }
+  }
+
 </style> 
